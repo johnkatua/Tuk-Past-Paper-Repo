b57d03735032053bba6f148b0734ea077d4d4805
john katua <johnkatua99@gmail.com>
2022-08-04 13:30:55 +0300

    added a footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="footer--container">Footer</div>;
+  return (
+    <footer className="footer--container">
+      &copy; 2022 All Copyrights Reserved. John Katua
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
+import image1 from "../assets/image-hero-desktop.png";
+
 const Welcome = () => {
   const navigate = useNavigate();
   return (
@@ -8,16 +10,22 @@ const Welcome = () => {
       <div className="welcome--container__details">
         <p className="welcome--title">Get instance access to past papers.</p>
         <p className="welcome--description">
-          No need to worry where to access past papers, we have all kind
-          of past papers for you. As a student we care about your productivity
-          and we want you and your colleagues to spend most of your time revising
-          rather than surfing the internet to find the right past paper to go through.
+          No need to worry where to access past papers, we have all kind of past
+          papers for you. As a student we care about your productivity and we
+          want you and your colleagues to spend most of your time revising
+          rather than surfing the internet to find the right past paper to go
+          through.
         </p>
-        <button className="welcome--button" onClick={() => navigate('/details')}>
+        <button
+          className="welcome--button"
+          onClick={() => navigate("/details")}
+        >
           Get started
         </button>
       </div>
-      <div className="welcome--container__image">B</div>
+      <div className="welcome--container__image">
+        <img src={image1} alt="welcome" title="credit to frontend mentor" />
+      </div>
     </div>
   );
 };
