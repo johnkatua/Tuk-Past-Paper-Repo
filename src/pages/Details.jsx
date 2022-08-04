c6a36957f53ab109c8bdc6762f566a954943998f
john katua <johnkatua99@gmail.com>
2022-08-04 14:11:14 +0300

    added dropdown to select courses

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -3,7 +3,16 @@ import React from "react";
 const Details = () => {
   return (
     <div className="details--container">
-      <div className="details--search__bar">A</div>
+      <div className="details--search__bar">
+        <input type="search" />
+        <div>
+          <label htmlFor="courses">Choose a course:</label>
+          <select name="courses" id="courses">
+            <option value="ABMI">ABMI</option>
+            <option value="ABME">ABME</option>
+          </select>
+        </div>
+      </div>
       <div className="details--table">B</div>
     </div>
   );
