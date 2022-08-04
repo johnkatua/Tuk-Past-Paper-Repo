a44bad381387d7be39c74b97a7bd5a5b8496f4e7
john katua <johnkatua99@gmail.com>
2022-08-04 14:36:00 +0300

    added table to the details component

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -6,14 +6,93 @@ const Details = () => {
       <div className="details--search__bar">
         <input type="search" />
         <div>
-          <label htmlFor="courses">Choose a course:</label>
           <select name="courses" id="courses">
             <option value="ABMI">ABMI</option>
             <option value="ABME">ABME</option>
           </select>
         </div>
+        <div>
+          <select name="courses" id="courses">
+            <option value="ABMI">FSST</option>
+            <option value="ABME">FAST</option>
+            <option value="ABME">FEBE</option>
+          </select>
+        </div>
       </div>
-      <div className="details--table">B</div>
+      <div className="details--table">
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Year</th>
+            <th>AcademicYear</th>
+            <th>Status</th>
+            <th>CourseCode</th>
+            <th>Level</th>
+            <th>Faculty</th>
+            <th>View</th>
+          </tr>
+          <tr>
+            <td>Name</td>
+            <td>Course</td>
+            <td>View</td>
+            <td>Name</td>
+            <td>Course</td>
+            <td>View</td>
+            <td>Name</td>
+            <td>Course</td>
+          </tr>
+          <tr>
+            <td>Name</td>
+            <td>Course</td>
+            <td>View</td>
+            <td>Name</td>
+            <td>Course</td>
+            <td>View</td>
+            <td>Name</td>
+            <td>Course</td>
+          </tr>
+           <tr>
+            <td>Name</td>
+            <td>Course</td>
+            <td>View</td>
+            <td>Name</td>
+            <td>Course</td>
+            <td>View</td>
+            <td>Name</td>
+            <td>Course</td>
+          </tr>
+          <tr>
+            <td>Name</td>
+            <td>Course</td>
+            <td>View</td>
+            <td>Name</td>
+            <td>Course</td>
+            <td>View</td>
+            <td>Name</td>
+            <td>Course</td>
+          </tr>
+           <tr>
+            <td>Name</td>
+            <td>Course</td>
+            <td>View</td>
+            <td>Name</td>
+            <td>Course</td>
+            <td>View</td>
+            <td>Name</td>
+            <td>Course</td>
+          </tr>
+          <tr>
+            <td>Name</td>
+            <td>Course</td>
+            <td>View</td>
+            <td>Name</td>
+            <td>Course</td>
+            <td>View</td>
+            <td>Name</td>
+            <td>Course</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 };
