ce14706cd28f83542a6ec36f73db459a1cccfe52
john katua <johnkatua99@gmail.com>
2022-08-04 12:30:39 +0300

    divided the welcome page into two parts

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -5,8 +5,17 @@ const Welcome = () => {
   const navigate = useNavigate();
   return (
     <div className="welcome--container">
-      Welcome
-      <button onClick={() => navigate("/details")}>Details page</button>
+      <div className="welcome--container__details">
+        <p>Access past papers</p>
+        <p>
+          Software development refers to a set of computer science activities
+          dedicated to the process of creating, designing, deploying and
+          supporting software. Software itself is the set of instructions or
+          programs that tell a computer what to do. It is independent of
+          hardware and makes computers programmable.
+        </p>
+      </div>
+      <div className="welcome--container__image">B</div>
     </div>
   );
 };
