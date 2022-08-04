8027b14cd19944ca167589abf33f1fb5af3215de
john katua <johnkatua99@gmail.com>
2022-08-04 13:00:31 +0300

    styled the description part of thetitle page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -6,14 +6,16 @@ const Welcome = () => {
   return (
     <div className="welcome--container">
       <div className="welcome--container__details">
-        <p>Access past papers</p>
-        <p>
-          Software development refers to a set of computer science activities
-          dedicated to the process of creating, designing, deploying and
-          supporting software. Software itself is the set of instructions or
-          programs that tell a computer what to do. It is independent of
-          hardware and makes computers programmable.
+        <p className="welcome--title">Get instance access to past papers.</p>
+        <p className="welcome--description">
+          No need to worry where to access past papers, we have all kind
+          of past papers for you. As a student we care about your productivity
+          and we want you and your colleagues to spend most of your time revising
+          rather than surfing the internet to find the right past paper to go through.
         </p>
+        <button className="welcome--button" onClick={() => navigate('/details')}>
+          Get started
+        </button>
       </div>
       <div className="welcome--container__image">B</div>
     </div>
