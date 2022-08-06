8b7ca9a42f9154a9e96142c4ca2dc5c7083e311f
john katua <johnkatua99@gmail.com>
2022-08-06 11:12:03 +0300

    formatted the app component

--- src/pages/Paper.jsx
+++ src/pages/Paper.jsx
@@ -3,20 +3,14 @@ import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "../features/paper/paperSlice";
 
 const Paper = () => {
-  const { value } = useSelector(state => state.paper);
+  const { value } = useSelector((state) => state.paper);
   const dispatch = useDispatch();
   return (
     <div className="paper--container">
       Paper
-      <button onClick={() => dispatch(increment())}>
-        +
-      </button>
-      <span>
-        {value}
-      </span>
-      <button onClick={() => dispatch(decrement())}>
-        -
-      </button>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <span>{value}</span>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 };
