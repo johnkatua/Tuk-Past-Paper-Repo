838cedf8e90256303585f2db45286bb73ccdd590
john katua <johnkatua99@gmail.com>
2022-08-06 11:59:39 +0300

    added client js file to be able to make get and post request to the backend

--- src/api/client.js
+++ src/api/client.js
@@ -1,3 +1,43 @@
-export async function client(params) {
-  
+export async function client(endpoint, { body, ...customConfig } = {}) {
+  const headers = {
+    'Content-Type': 'application/json'
+  };
+
+  const config = {
+    method: body ? 'POST' : 'GET',
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers,
+    },
+  };
+
+  if (body) {
+    config.body = JSON.stringify(body);
+  };
+
+  let data;
+  try {
+    const response = await window.fetch(endpoint, config);
+    data = await response.json();
+    if (response.ok) {
+      return {
+        status: response.status,
+        data,
+        headers: response.headers,
+        url: response.url
+      }
+    }
+    throw new Error(response.statusText);
+  } catch (error) {
+    return Promise.reject(error.message ? error.message : data);
+  }
+};
+
+client.get = function (endpoint, body, customConfig = {}) {
+  return client(endpoint, { ...customConfig, method: 'GET' });
+};
+
+client.post = function (endpoint, body, customConfig = {}) {
+  return client(endpoint, { ...customConfig, body });
 }
