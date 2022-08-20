13b8daeb94e014533978d69e8055a65ea66c374a
john katua <johnkatua99@gmail.com>
2022-08-21 01:35:36 +0300

    imported the functionality to fetch a single paper

--- src/pages/SinglePaperPage.jsx
+++ src/pages/SinglePaperPage.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectPaperById } from '../features/paper/paperSlice';
 
-const SinglePaperPage = () => {
+const SinglePaperPage = ({ match }) => {
+  const { paperId } = match.params;
+  const paper = useSelector(state => selectPaperById(state, paperId))
   return (
     <div>SinglePaperPage</div>
   )
