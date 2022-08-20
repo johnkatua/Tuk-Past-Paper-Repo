a10a766666c40f944140a9358a53bf1fe7c71bbe
john katua <johnkatua99@gmail.com>
2022-08-21 01:17:16 +0300

    added the method to get all papers and a single paper

--- src/features/paper/paperSlice.js
+++ src/features/paper/paperSlice.js
@@ -5,3 +5,9 @@ export const paperSlice = createSlice({});
 export const { paperAdded, paperUpdated } = paperSlice.actions;
 
 export default paperSlice.reducer;
+
+export const selectAllPapers = state => state.papers;
+
+export const selectPaperById = (state, paperId) => {
+  return state.papers.find(paper => paper.id === paperId)
+};
