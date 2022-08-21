86dbc5c76553b7cfac7c2abce2d35bcfafd9bcb3
john katua <johnkatua99@gmail.com>
2022-08-21 13:49:56 +0300

    fetched papers successfully from backend

--- src/pages/PaperExcerpt.jsx
+++ src/pages/PaperExcerpt.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const PaperExcerpt = () => {
+const PaperExcerpt = ({ paper }) => {
   return (
-    <div>PaperExcerpt</div>
+    <article>
+      <h3>{paper.name}</h3>
+      <p>{paper.year}</p>
+      <p>{paper.status}</p>
+      <p>{paper.faculty}</p>
+      <p>{paper.academicYear}</p>
+      <p>{paper.courseCode}</p>
+      <p>{paper.courseLevel}</p>
+    </article>
   )
 }
 
--- src/pages/PaperList.jsx
+++ src/pages/PaperList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectAllPapers, fetchPapers } from '../features/paper/paperSlice';
+import PaperExcerpt from './PaperExcerpt';
 
 const PaperList = () => {
   const dispatch = useDispatch();
@@ -13,9 +14,20 @@ const PaperList = () => {
       dispatch(fetchPapers());
     }
   }, [paperStatus, dispatch]);
+
+  let content;
+
+  if (paperStatus === 'succeeded') {
+    const fetchedPapers = papers.papers[0].data;
+    content = fetchedPapers.map(paper => (
+      <PaperExcerpt key={paper.name} paper={paper} />
+    ))
+  }
   
   return (
-    <div>PaperList</div>
+    <div>
+      {content}
+    </div>
   )
 }
 
