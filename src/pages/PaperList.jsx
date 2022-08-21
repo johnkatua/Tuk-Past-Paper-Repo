6000c77b325026a59cd6ed072f467d7896ea4681
john katua <johnkatua99@gmail.com>
2022-08-21 14:09:16 +0300

    added table to view paper details

--- src/pages/PaperList.jsx
+++ src/pages/PaperList.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectAllPapers, fetchPapers } from '../features/paper/paperSlice';
+import Details from './Details';
 import PaperExcerpt from './PaperExcerpt';
 
 const PaperList = () => {
   const dispatch = useDispatch();
   const papers = useSelector(selectAllPapers);
-  console.log(papers);
   const paperStatus = useSelector(state => state.papers.status);
 
   useEffect(() => {
@@ -20,7 +20,7 @@ const PaperList = () => {
   if (paperStatus === 'succeeded') {
     const fetchedPapers = papers.papers[0].data;
     content = fetchedPapers.map(paper => (
-      <PaperExcerpt key={paper.name} paper={paper} />
+      <Details key={paper.name} paper={paper} />
     ))
   }
   
