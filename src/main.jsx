6f4aca1f0423790265e455fabd8a6ee44d275869
john katua <johnkatua99@gmail.com>
2022-08-22 08:33:01 +0300

    changed the font-weight of the title in welcome page

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import store from "./store";
 import { Provider } from "react-redux";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
