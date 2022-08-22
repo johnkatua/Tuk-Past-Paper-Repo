baf87f090e6214cc9499ade6d66467923c24d144
john katua <johnkatua99@gmail.com>
2022-08-23 02:10:51 +0300

    added a modal slice to the store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import modalSlice from "./features/modal/modalSlice";
 import paperSlice from "./features/paper/paperSlice";
 
 export default configureStore({
   reducer: {
     papers: paperSlice,
+    modal: modalSlice
   },
 });
