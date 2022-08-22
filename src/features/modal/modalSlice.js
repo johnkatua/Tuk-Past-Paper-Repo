752fee137a7829d04a7e87a7777d2cfa1482c22e
john katua <johnkatua99@gmail.com>
2022-08-23 02:01:29 +0300

    added actions to open and close modal

--- src/features/modal/modalSlice.js
+++ src/features/modal/modalSlice.js
@@ -3,3 +3,16 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   show: false
 };
+
+export const modalSlice = createSlice({
+  name: 'modal',
+  initialState,
+  reducers: {
+    openModal: state => {
+      state.show = true
+    },
+    closeModal: state => {
+      state.show = false
+    }
+  }
+});
