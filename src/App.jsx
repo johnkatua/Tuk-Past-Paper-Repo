12ca6b4a0dc282524ab8068b01bef5ca2d6252f9
john katua <johnkatua99@gmail.com>
2022-08-23 03:18:39 +0300

    added worker to the index file of the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Worker } from "@react-pdf-viewer/core";
 import "./App.css";
 import Pages from "./pages/index";
 
 const App = () => {
-  return <Pages />;
+  return (
+    <Worker>
+      <Pages />
+    </Worker>
+  );
 };
 
 export default App;
