229b771678154fe88654347c885df64bbd9e1f4c
john katua <johnkatua99@gmail.com>
2022-08-27 01:23:24 +0300

    added styles to the registration page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="auth--container">
+      <div className="auth--container__card">
+        <h3>Register</h3>
+        <div className="auth--card__firstrow">
+          <input type='text' />
+          <input type='text' />
+        </div>
+        <div className="auth--card__secondrow">
+          <input type='email' />
+        </div>
+        <div className="auth--card__thirdrow">
+          <input type='password' />
+        </div>
+        <div>
+          <button>
+            Create an account
+          </button>
+        </div>
+        <div>
+          <span>Do not have an account.</span>
+          {" "}
+          <span>Register Here</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
