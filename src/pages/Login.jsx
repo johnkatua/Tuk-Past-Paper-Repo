8009fda2962ba4b3b922264767993f2bcb8a0ab6
john katua <johnkatua99@gmail.com>
2022-08-27 00:55:33 +0300

    added inputs to capture user data while signing in to the system

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,27 @@ const Login = () => {
   return (
     <div className="login--container">
       <div className="login--container__card">
-        A
+        <h3>Login</h3>
+        <div>
+          <input type='text' />
+          <input type='text' />
+        </div>
+        <div>
+          <input type='email' />
+        </div>
+        <div>
+          <input type='password' />
+        </div>
+        <div>
+          <button>
+            Create an account
+          </button>
+        </div>
+        <div>
+          <span>Already have an account.</span>
+          {" "}
+          <span>Login Here</span>
+        </div>
       </div>
     </div>
   );
