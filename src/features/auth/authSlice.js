428254f2d57221575b8e959e62d1cb09bec9e4ce
john katua <johnkatua99@gmail.com>
2022-08-27 12:32:08 +0300

    added a case to register user

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -10,13 +10,21 @@ export const userLogin = createAsyncThunk('auth/userLogin', async (values) => {
   return response.data;
 });
 
+export const userRegister = createAsyncThunk('auth/userRegister', async (values) => {
+  const response = await client.post("http://localhost:4001/signup", values);
+  return response.data;
+})
+
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   extraReducers(builder) {
     builder.addCase(userLogin.fulfilled, (state, action) => {
       state.auth = action.payload;
-    })
+    });
+    builder.addCase(userRegister.fulfilled, (state, action) => {
+      state.auth = action.payload;
+    });
   }
 });
 
