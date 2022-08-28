8a7c1c28507e01efca8a5564cb46a4b07b7945f6
john katua <johnkatua99@gmail.com>
2022-08-28 10:31:52 +0300

    added a function to open and close modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { closeModal, openModal } from "../features/modal/modalSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
   const { show } = useSelector(state => state.modal);
   console.log(show);
   const navigate = useNavigate();
+
+  const handleToolTip = () => {
+    if (show) {
+      dispatch(closeModal());
+    } else {
+      dispatch(openModal())
+    }
+  }
+
   return (
     <div className="header--container">
       <h1 onClick={() => navigate("/")}>Tuk Past Past Repo</h1>
       {user ? (
-        <div>{user}</div>
+        <div onClick={handleToolTip}>{user}</div>
       ) : (
         <div className="header--btns">
           <button
