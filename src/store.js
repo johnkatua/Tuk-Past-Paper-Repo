4b398949dc2d11b5dd7214d0d448a5726dd14b64
john katua <johnkatua99@gmail.com>
2022-08-28 12:40:53 +0300

    added tooltip slice to the redux store so as it is available in the whole application

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./features/auth/authSlice";
 import modalSlice from "./features/modal/modalSlice";
 import paperSlice from "./features/paper/paperSlice";
+import toolTipSlice from "./features/tooltip/toolTip";
 
 export default configureStore({
   reducer: {
     papers: paperSlice,
     modal: modalSlice,
     auth: authSlice,
+    toolTip: toolTipSlice
   },
 });
