3832c2227c5e6266fffb453b47a8b3be41b255c3
john katua <johnkatua99@gmail.com>
2022-08-28 12:05:24 +0300

    styled the tooltip

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Tooltip = () => {
   return (
-    <div className='tooltip--container'>Tooltip</div>
+    <div className='tooltip--container'>
+      <div className='tooltip--details'>Account Settings</div>
+      <hr />
+      <div className='tooltip--details'>
+        My papers
+      </div>
+      <hr />
+      <div className='tooltip--details'>
+        Logout
+      </div>
+    </div>
   )
 }
 
