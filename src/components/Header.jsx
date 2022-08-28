0d1c6bd9f7a484d4ad66aa66fe3ebd41926345ab
john katua <johnkatua99@gmail.com>
2022-08-28 12:47:46 +0300

    fixed the error in details page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,26 +3,28 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { closeModal, openModal } from "../features/modal/modalSlice";
 import Tooltip from "./Tooltip";
+import { closeToolTip, openToolTip } from "../features/tooltip/toolTip";
 
 const Header = () => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
   const { show } = useSelector(state => state.modal);
+  const { showToolTip } = useSelector(state => state.toolTip);
   console.log(show);
   const navigate = useNavigate();
 
   const handleToolTip = () => {
-    if (show) {
-      dispatch(closeModal());
+    if (showToolTip) {
+      dispatch(closeToolTip());
     } else {
-      dispatch(openModal())
+      dispatch(openToolTip());
     }
   }
 
   return (
     <div className="header--container">
       <h1 onClick={() => navigate("/")}>Tuk Past Past Repo</h1>
-      {show && <Tooltip />}
+      {showToolTip && <Tooltip />}
       {user ? (
         <div onClick={handleToolTip}>{user}</div>
       ) : (
