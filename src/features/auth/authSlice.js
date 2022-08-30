f78489684512d22426e21cd486fa1030e8370823
john katua <johnkatua99@gmail.com>
2022-08-30 12:30:59 +0300

    added a toast error to the user registration action

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { Cookies } from "react-cookie";
+import { toast } from "react-toastify";
 import axios from "axios";
 
 const cookies = new Cookies();
@@ -23,6 +24,7 @@ export const userLogin = createAsyncThunk(
       cookies.set("user", response.data.data.email, { path: "/" });
       return response.data;
     } catch (error) {
+      toast.error('error')
       return rejectWithValue(error.response.data.msg);
     }
   }
@@ -37,6 +39,7 @@ export const userRegister = createAsyncThunk(
       cookies.set("user", response.data.data.email, { path: "/" });
       return response.data;
     } catch (error) {
+      toast.error(error.response.data.msg);
       return rejectWithValue(error.response.data.msg);
     }
   }
