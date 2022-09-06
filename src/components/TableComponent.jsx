a08f151aab753f8a26a7dbe633e6484fa9a9a1be
john katua <johnkatua99@gmail.com>
2022-09-06 09:26:02 +0300

    added a table component

---
+++ src/components/TableComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+
+const TableComponent = () => {
+  return (
+    <div>TableComponent</div>
+  )
+}
+
+export default TableComponent
