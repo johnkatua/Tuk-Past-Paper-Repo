1090e9ab8c7c2f63006ac875ad5f6d78465daadc
john katua <johnkatua99@gmail.com>
2022-09-06 16:18:06 +0300

    added a fav slice

--- src/features/favorite/favoriteSlice.js
+++ src/features/favorite/favoriteSlice.js
@@ -1,1 +1,31 @@
-console.log('fav');
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  favPapers: [],
+  status: 'idle'
+};
+
+export const fetchFavoritePapers = createAsyncThunk(
+  "favPapers/fetchFavoritePapers",
+  async (userId) => {
+    const response = await axios.get(
+      `http://localhost:4001/fav/getAllFav/${userId}`
+    );
+    return response.data;
+  }
+);
+
+export const favSlice = createSlice({
+  name: "favPapers",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchFavoritePapers.fulfilled, (action, state) => {
+      state.status = 'success',
+      state.favPapers = action.payload;
+    })
+  }
+});
+
+export default favSlice.reducer;
+
