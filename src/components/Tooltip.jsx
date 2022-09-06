aa21d637501aa0a3db461117d21f12ebece71864
john katua <johnkatua99@gmail.com>
2022-09-06 07:42:01 +0300

    user can navigate successfully to the fav page

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Tooltip = ({ logout }) => {
+  const navigate = useNavigate();
   return (
     <div className="tooltip--container">
       <div className="tooltip--details">Account Settings</div>
       <hr />
-      <div className="tooltip--details">My papers</div>
+      <div className="tooltip--details" onClick={() => navigate('/favorite')}>My papers</div>
       <hr />
       <div className="tooltip--details" onClick={logout}>
         Logout
