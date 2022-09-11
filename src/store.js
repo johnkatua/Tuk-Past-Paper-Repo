80a9ece7e279465fd8cc0a3447a2b0d6f7356bf0
john katua <johnkatua99@gmail.com>
2022-09-11 23:19:38 +0300

    added  faculty slice to the store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./features/auth/authSlice";
+import facultySlice from "./features/faculty/facultySlice";
 import favSlice from "./features/favorite/favoriteSlice";
 import modalSlice from "./features/modal/modalSlice";
 import paperSlice from "./features/paper/paperSlice";
@@ -12,5 +13,6 @@ export default configureStore({
     auth: authSlice,
     toolTip: toolTipSlice,
     favPapers: favSlice,
+    faculty: facultySlice
   },
 });
