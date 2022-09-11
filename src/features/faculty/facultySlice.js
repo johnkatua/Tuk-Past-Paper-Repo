fcf85f9bc9f2822be70fc19162854334661749a9
john katua <johnkatua99@gmail.com>
2022-09-11 23:37:03 +0300

    added an action to fetch faculties

--- src/features/faculty/facultySlice.js
+++ src/features/faculty/facultySlice.js
@@ -1,14 +1,24 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   faculties: []
 };
 
+export const fetchFaculties = createAsyncThunk('faculty/fetchFaculties', async () => {
+  const response = await axios.get('http://localhost:4001/faculty/getAllFaculties');
+  return response.data;
+});
+
 export const facultySlice = createSlice({
   name: 'faculty',
   initialState,
   reducers: {},
-  extraReducers: {}
+  extraReducers(builder) {
+    builder.addCase(fetchFaculties.fulfilled, (state, action) => {
+      state.faculties = action.payload.data;
+    })
+  }
 });
 
 export default facultySlice.reducer;
