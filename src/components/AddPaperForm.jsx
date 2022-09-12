04a41ada3aaa4b2121fc0c6cd0819aac886ee622
john katua <johnkatua99@gmail.com>
2022-09-12 19:10:34 +0300

    added a different input to the create modal form

--- src/components/AddPaperForm.jsx
+++ src/components/AddPaperForm.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Form, Col, Row } from "react-bootstrap";
 
 const AddPaperForm = (props) => {
-  console.log(props);
   return (
     <Form>
       <Row className="mb-4">
@@ -10,27 +9,27 @@ const AddPaperForm = (props) => {
           <Form.Control type="text" placeholder="Name" value={props.name} onChange={props.handleChange} />
         </Col>
         <Col>
-          <Form.Control placeholder="File" />
+          <Form.Control type="file" placeholder="File" formEncType="multipart/form-data" onChange={props.saveFile} />
         </Col>
       </Row>
       <Row className="mb-4">
         <Col>
-          <Form.Control placeholder="Year" />
+          <Form.Control placeholder="Year" type="text" value={props.year} onChange={props.handleChange} />
         </Col>
         <Col>
-          <Form.Control placeholder="Academic Year" />
+          <Form.Control placeholder="Academic Year" type="text" value={props.academicYear} onChange={props.handleChange} />
         </Col>
       </Row>
       <Row className="mb-4">
         <Col>
-          <Form.Select>
+          <Form.Select value={props.status} onChange={props.handleChange}>
             <option>Open this select menu</option>
             <option>Main Exam</option>
             <option>Cat</option>
           </Form.Select>
         </Col>
         <Col>
-          <Form.Select>
+          <Form.Select value={props.facultyId} onChange={props.handleChange}>
             <option>Open this select menu</option>
             <option>FSST</option>
             <option>FEBE</option>
@@ -40,7 +39,7 @@ const AddPaperForm = (props) => {
       </Row>
       <Row className="mb-4">
         <Col>
-          <Form.Select>
+          <Form.Select value={props.courseId} onChange={props.handleChange}>
             <option>Open this select menu</option>
             <option>Degree</option>
             <option>Diploma</option>
