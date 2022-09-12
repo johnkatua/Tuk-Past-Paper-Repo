11a9d75793c2b129f1fd7967ccce68ee1964e325
john katua <johnkatua99@gmail.com>
2022-09-12 08:25:41 +0300

    created a fetch course thunk to enable communication between redux and  api

--- src/features/course/courseSlice.js
+++ src/features/course/courseSlice.js
@@ -1,8 +1,23 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { Cookies } from "react-cookie";
+
+const cookie = new Cookies();
+
+const token = cookie.get('token');
 
 const initialState = {
   courses: [],
-}
+};
+
+export const fetchCourses = createAsyncThunk('courses/fetchCourses', async () => {
+  const response = await axios.get('http://localhost:4001/courses/getAllCourses', {
+    headers: {
+      authorization: `Bearer ${token}`
+    }
+  })
+  return response.data
+})
 
 export const courseSlice = createSlice({
   name: 'courses',
