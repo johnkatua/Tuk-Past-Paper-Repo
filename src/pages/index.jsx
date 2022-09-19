5f67f2aa901313a88168dfbf87b9b239fc4b97b7
john katua <johnkatua99@gmail.com>
2022-09-19 22:02:31 +0300

    cleaned the route file

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { Cookies } from "react-cookie";
 import Layout from "../components/Layout";
 import Welcome from "./Welcome";
 import "./index.css";
@@ -10,26 +8,12 @@ import PaperList from "./PaperList";
 import Login from "./Login";
 import Register from "./Register";
 import FavoriteList from "./FavoriteList";
-import { setUser } from "../features/auth/authSlice";
-
-const cookies = new Cookies();
-
-const user = cookies.get("token");
-
-console.log(user);
 
 const LayoutRoute = ({ children }) => {
   return <Layout>{children}</Layout>;
 };
 
 const Pages = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    if (user) {
-      dispatch(setUser(user));
-    }
-  }, [user]);
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Router>
