8b8dbeeaf6e4aa54d31983de8e480db24529aef3
john katua <johnkatua99@gmail.com>
2022-09-19 14:48:15 +0300

    removed unnecessary code from app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import { useEffect } from "react";
 import { Worker } from "@react-pdf-viewer/core";
-import { CookiesProvider, Cookies } from "react-cookie";
-import { useDispatch, useSelector } from "react-redux";
+import { CookiesProvider } from "react-cookie";
+import { useDispatch } from "react-redux";
 import "./App.css";
 import Pages from "./pages/index";
 import "@react-pdf-viewer/core/lib/styles/index.css";
-import { setToken, getUserProfile } from "./features/auth/authSlice";
+import { getUserProfile } from "./features/auth/authSlice";
 
 const App = () => {
-  const cookie = new Cookies();
-  const { user } = useSelector((state) => state.auth);
-  console.log('user', user);
   const dispatch = useDispatch();
   const token = localStorage.getItem('token');
-  console.log('token', token);
   useEffect(() => {
     if (token) {
       dispatch(getUserProfile());
