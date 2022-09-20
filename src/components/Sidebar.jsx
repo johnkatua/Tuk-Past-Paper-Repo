f358676d7438a0c31108485b5a18f503ac873afe
john katua <johnkatua99@gmail.com>
2022-09-20 15:32:22 +0300

    added a menu list in the sidebar file

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { FaSchool } from 'react-icons/fa';
+import { IoNewspaperSharp, IoList } from 'react-icons/io';
+
+const menus = [
+  {
+    name: 'Faculties',
+    icon: <FaSchool />,
+    path: '/admin-dashboard',
+  },
+  {
+    name: 'Courses',
+    icon: <IoList />,
+    path: '/admin-dashboard',
+  },
+  {
+    name: 'Papers',
+    icon: <IoNewspaperSharp />,
+    path: '/admin-dashboard',
+  }
+]
 
 const Sidebar = () => {
   return (
