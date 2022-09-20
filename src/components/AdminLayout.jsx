90d49acf9ca7163618541d23ed16d1465ac04cd2
john katua <johnkatua99@gmail.com>
2022-09-20 10:22:13 +0300

    added a show state to control the sidebar

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { useState } from 'react'
 
 const AdminLayout = () => {
+  const [show, setShow] = useState(false);
+  console.log(show);
   return (
     <div className='admin--layout'>
-      <div className='admin--layout__header'>Header</div>
+      <div className='admin--layout__header' onClick={(state) => setShow(!state)}>Header</div>
       <div className='admin--layout__body'>
         <div className='admin--sidebar'>Sidebar</div>
         <div className='admin--content'>Children</div>
