ea47bfd6fda0a47a68e22957c76e360747a5ae3b
john katua <johnkatua99@gmail.com>
2022-09-21 12:37:13 +0300

    added a form to capture faculty details

--- src/pages/Admin/Faculties/FacultyDetails.jsx
+++ src/pages/Admin/Faculties/FacultyDetails.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const FacultyDetails = () => {
-  return <div className="admin--dashboard__details">FacultyDetails</div>;
+  return (
+    <div className="admin--dashboard__details">
+      <div>Header</div>
+      <div className="form--container__group">
+        <label>Name</label>
+        <input type="text" name="name" placeholder="name" />
+      </div>
+      <div className="form--container__group">
+        <label>Acronym</label>
+        <input type="text" name="acronym" placeholder="acronym" />
+      </div>
+      <div className="form--container__group">
+        <label>Description</label>
+        <textarea placeholder="description"></textarea>
+      </div>
+    </div>
+    );
 };
 
 export default FacultyDetails;
