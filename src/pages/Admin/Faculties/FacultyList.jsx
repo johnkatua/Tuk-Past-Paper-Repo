babeb94c128d8d84860aa30f37a3ccd6c6615413
john katua <johnkatua99@gmail.com>
2022-09-21 10:04:42 +0300

    fetching faculties from the api

--- src/pages/Admin/Faculties/FacultyList.jsx
+++ src/pages/Admin/Faculties/FacultyList.jsx
@@ -1,8 +1,16 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFaculties } from "../../../features/faculty/facultySlice";
 
 const FacultyList = () => {
+  const dispatch = useDispatch();
   const { faculties } = useSelector((state) => state.faculty);
-  console.log(faculties);
+  useEffect(() => {
+    if (faculties.length === 0) {
+      dispatch(fetchFaculties());
+    }
+  }, [faculties]);
+
   return (
     <div className="admin--dashboard__list">
       <h3 className="admin--list__header">Faculties</h3>
