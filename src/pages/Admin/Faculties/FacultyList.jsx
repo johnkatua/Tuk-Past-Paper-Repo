4ec86916be42de546ac99a544fea8a58c4983f53
john katua <johnkatua99@gmail.com>
2022-09-21 09:43:12 +0300

    added faculty header and its children

--- src/pages/Admin/Faculties/FacultyList.jsx
+++ src/pages/Admin/Faculties/FacultyList.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const FacultyList = () => {
-  return <div className="admin--dashboard__list">FacultyList</div>;
+  return (
+    <div className="admin--dashboard__list">
+      <h3 className="admin--list__header">Faculties</h3>
+      <div className="admin--list">A</div>
+    </div>
+  );
 };
 
 export default FacultyList;
