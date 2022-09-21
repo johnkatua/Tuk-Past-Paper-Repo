f83f7619f44ba952f967b795b0888e572d4449a8
john katua <johnkatua99@gmail.com>
2022-09-21 15:51:47 +0300

    capturing faculty details

--- src/pages/Admin/Faculties/FacultyDetails.jsx
+++ src/pages/Admin/Faculties/FacultyDetails.jsx
@@ -4,33 +4,36 @@ import { useSelector } from "react-redux";
 const FacultyDetails = () => {
   const [title, setTitle] = useState('Select a faculty')
   const { faculty } = useSelector((state) => state.faculty);
-  console.log(faculty);
+  const [item, setItem] = useState({
+    name: faculty?.name,
+    acronym: faculty?.acronym,
+    description: faculty?.description
+  })
+
+  console.log(item);
 
   useEffect(() => {
     if (faculty !== null) {
       setTitle(faculty.name);
     }
   }, [title, faculty])
-
-  console.log(title);
   
   return (
     <div className="admin--dashboard__details">
       <div className="details--header">
         {title}
-        {/* {faculty == {} ? (<h3>{faculty.name}</h3>) : (<h2>Select a faculty</h2>)} */}
       </div>
       <div className="form--container__group">
         <label>Name</label>
-        <input type="text" name="name" placeholder="name" />
+        <input type="text" name="name" placeholder="name" value={item.name} />
       </div>
       <div className="form--container__group">
         <label>Acronym</label>
-        <input type="text" name="acronym" placeholder="acronym" />
+        <input type="text" name="acronym" placeholder="acronym" value={item.acronym} />
       </div>
       <div className="form--container__group">
         <label>Description</label>
-        <textarea placeholder="description"></textarea>
+        <textarea placeholder="description" name="description" value={item.description}></textarea>
       </div>
       <div className="form--container__btns">
         <button className="details--btn">Save</button>
