8f56d046349daa7012843d2fd964743ab50d1d64
john katua <johnkatua99@gmail.com>
2022-09-28 10:48:54 +0300

    added a span to checkbox labels

--- src/pages/Admin/Paper/PaperDetails.jsx
+++ src/pages/Admin/Paper/PaperDetails.jsx
@@ -50,11 +50,11 @@ const PaperDetails = () => {
         <div>Select Paper Status</div>
         <div className="form--container__status">
           <div className="form--status__firstRow">
-            MainExam
+            <span>Main Exam</span>
             <input type="radio" value="mainExam" name="status" />
           </div>
           <div className="form--status__secondRow">
-            Cat
+            <span>Cat</span>
             <input type="radio" value="cat" name="status" />
           </div>
         </div>
@@ -63,20 +63,29 @@ const PaperDetails = () => {
       <div className="form--container__group">
         <div>Select Paper Year</div>
         <div className="form--container__year">
-          <div>
-            <input type="radio" value="firstYear" name="year" />
-            First Year
-            <input type="radio" value="secondYear" name="year" />
-            Second Year
+          <div className="form--year__firstRow">
+            <div className="form--year__firstYear">
+              <span>First Year</span>
+              <input type="radio" value="firstYear" name="year" />
+            </div>
+            <div className="form--year__secondYear">
+              <span>Second Year</span>
+              <input type="radio" value="secondYear" name="year" />
+            </div>
           </div>
-          <div>
-            <input type="radio" value="thirdYear" name="year" />
-            Third Year
-            <input type="radio" value="fourthYear" name="year" />
-            Fourth Year
+          <div className="form--year__secondRow">
+            <div className="form--year__firstYear">
+              <span>Third Year</span>
+              <input type="radio" value="thirdYear" name="year" />
+            </div>
+            <div className="form--year__secondYear">
+              <span>Fourth Year</span>
+              <input type="radio" value="fourthYear" name="year" />
+            </div>
           </div>
         </div>
       </div>
+      <hr />
       <div className="form--container__btns">
         <button className="details--btn" onClick={() => handleSubmit()}>
           Save
