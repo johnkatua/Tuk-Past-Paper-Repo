2be9ba7dfb12e00c3797343acf1c51caf2b1e464
john katua <johnkatua99@gmail.com>
2022-09-29 12:26:45 +0300

    fetching courses from the store

--- src/pages/Admin/Courses/CourseList.jsx
+++ src/pages/Admin/Courses/CourseList.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const CourseList = () => {
+  const dispatch =  useDispatch();
+  const { courses } = useSelector((state) => state.courses);
   return (
-    <div className='admin--dashboard__list'>CourseList</div>
+    <div className='admin--dashboard__list'>
+      <h3 className='admin--list__header'>Papers</h3>
+      <ul></ul>
+    </div>
   )
 }
 
 export default CourseList
+
+{/* <div className="admin--dashboard__list">
+      <h3 className="admin--list__header">Papers</h3>
+      <ul>
+        {papers.map((paper) => (
+          <li key={paper.id}>
+            <div
+              className="admin--list"
+              onClick={() => dispatch(getSelectedPaper(paper))}
+            >
+              <div className="list--header">{paper.name}</div>
+              <div className="list--details">{paper.status}</div>
+            </div>
+            <IconContext.Provider
+              value={{ color: "red", size: "1.2rem", title: "Delete" }}
+            >
+              <div
+                className="list--delete"
+                onClick={() => dispatch(deletePaper({id: paper.id}))}
+              >
+                <AiOutlineDelete />
+              </div>
+            </IconContext.Provider>
+          </li>
+        ))}
+      </ul>
+    </div> */}
