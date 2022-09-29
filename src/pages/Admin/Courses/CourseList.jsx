61841a0f199e147f74458384b4b54edefde30500
john katua <johnkatua99@gmail.com>
2022-09-29 12:42:21 +0300

    fetching courses when the page mounts

--- src/pages/Admin/Courses/CourseList.jsx
+++ src/pages/Admin/Courses/CourseList.jsx
@@ -2,10 +2,16 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { IconContext } from 'react-icons/lib';
 import { AiOutlineDelete } from 'react-icons/ai';
+import { fetchCourses } from '../../../features/course/courseSlice';
 
 const CourseList = () => {
   const dispatch =  useDispatch();
   const { courses } = useSelector((state) => state.courses);
+
+  useEffect(() => {
+    dispatch(fetchCourses());
+  }, []);
+  
   return (
     <div className='admin--dashboard__list'>
       <h3 className='admin--list__header'>Papers</h3>
@@ -28,31 +34,4 @@ const CourseList = () => {
   )
 }
 
-export default CourseList
-
-{/* <div className="admin--dashboard__list">
-      <h3 className="admin--list__header">Papers</h3>
-      <ul>
-        {papers.map((paper) => (
-          <li key={paper.id}>
-            <div
-              className="admin--list"
-              onClick={() => dispatch(getSelectedPaper(paper))}
-            >
-              <div className="list--header">{paper.name}</div>
-              <div className="list--details">{paper.status}</div>
-            </div>
-            <IconContext.Provider
-              value={{ color: "red", size: "1.2rem", title: "Delete" }}
-            >
-              <div
-                className="list--delete"
-                onClick={() => dispatch(deletePaper({id: paper.id}))}
-              >
-                <AiOutlineDelete />
-              </div>
-            </IconContext.Provider>
-          </li>
-        ))}
-      </ul>
-    </div> */}
+export default CourseList;
