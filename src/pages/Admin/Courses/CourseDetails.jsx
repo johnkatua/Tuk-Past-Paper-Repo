d323434f3c4ffefabfac453673462922276b1891
john katua <johnkatua99@gmail.com>
2022-09-29 13:43:42 +0300

    changed the checked value to default checked

--- src/pages/Admin/Courses/CourseDetails.jsx
+++ src/pages/Admin/Courses/CourseDetails.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const CourseDetails = () => {
   const [title, setTitle] = useState('Select a course');
+  const { faculties } = useSelector((state) => state.faculty);
   const [item, setItem] = useState({
     name: '',
     courseCode: '',
@@ -14,7 +16,8 @@ const CourseDetails = () => {
       ...item,
       [e.target.name]: e.target.value 
     }))
-  }
+  };
+
   return (
     <div className='admin--dashboard__details'>
       <div className='details--header'>
@@ -22,8 +25,38 @@ const CourseDetails = () => {
       </div>
       <div className='form--container__group'>
         <label>Name</label>
-        <input type="text" name='name' placeholder='Enter course name' />
+        <input type="text" name='name' placeholder='Enter course name' value={item.name} onChange={handleChange} />
       </div>
+      <div className='form--container__group'>
+        <label>Course Code</label>
+        <input type="text" name='courseCode' placeholder='Enter course code e.g ABBQ' value={item.courseCode} onChange={handleChange} />
+      </div>
+      <div className="form--container__group" value={item.status} onChange={handleChange}>
+        <div>Select Course Status</div>
+        <div className="form--container__status">
+          <div className="form--status__firstRow">
+            <span>Degree</span>
+            <input type="radio" value="degree" name="status" defaultChecked={item.status === "degree"} />
+          </div>
+          <div className="form--status__secondRow">
+            <span>Diploma</span>
+            <input type="radio" value="diploma" name="status" defaultChecked={item.status === "diploma"} />
+          </div>
+        </div>
+      </div>
+      <hr />
+       {/* <div className="form--container__group" value={item.facultyId} onChange={handleChange}>
+        <div>Select Faculty</div>
+        <div className="form--container__faculties">
+          {faculties.map((faculty) => (
+            <div className="form--faculties">
+              <span>{faculty.acronym}</span>
+              <input type="radio" value={faculty._id} name="facultyId" checked={item.facultyId === faculty._id} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <hr /> */}
     </div>
   )
 }
