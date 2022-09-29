4b01cf68d5ee3b7b5455407742def5da9e3628bc
john katua <johnkatua99@gmail.com>
2022-09-29 13:09:01 +0300

    added a handle change function

--- src/pages/Admin/Courses/CourseDetails.jsx
+++ src/pages/Admin/Courses/CourseDetails.jsx
@@ -2,10 +2,27 @@ import { useState } from 'react'
 
 const CourseDetails = () => {
   const [title, setTitle] = useState('Select a course');
+  const [item, setItem] = useState({
+    name: '',
+    courseCode: '',
+    status: '',
+    facultyId: ''
+  });
+
+  const handleChange = () => {
+    setItem((item) => ({
+      ...item,
+      [e.target.name]: e.target.value 
+    }))
+  }
   return (
     <div className='admin--dashboard__details'>
       <div className='details--header'>
         {title}
+      </div>
+      <div className='form--container__group'>
+        <label>Name</label>
+        <input type="text" name='name' placeholder='Enter course name' />
       </div>
     </div>
   )
