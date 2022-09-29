959e898ec73a4ead75114aa4ef6daba9be7f5762
john katua <johnkatua99@gmail.com>
2022-09-29 14:35:15 +0300

    removed the selected paper declaration

--- src/pages/Admin/Paper/PaperDetails.jsx
+++ src/pages/Admin/Paper/PaperDetails.jsx
@@ -10,7 +10,6 @@ const PaperDetails = () => {
   const { courses } = useSelector((state) => state.courses);
   const { paper } = useSelector((state) => state.papers);
   const [title, setTitle] = useState("Select a paper");
-  const [selectedPaper, setSelectedPaper] = useState(null);
   const [paperFile, setPaperFile] = useState(null);
   const [item, setItem] = useState({
     name: "",
