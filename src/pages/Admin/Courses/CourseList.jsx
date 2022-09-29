f2c91f9726a68a5e26e262e868f2b2f59b96026c
john katua <johnkatua99@gmail.com>
2022-09-29 12:40:26 +0300

    displaying courses from the api

--- src/pages/Admin/Courses/CourseList.jsx
+++ src/pages/Admin/Courses/CourseList.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { IconContext } from 'react-icons/lib';
+import { AiOutlineDelete } from 'react-icons/ai';
 
 const CourseList = () => {
   const dispatch =  useDispatch();
@@ -7,7 +9,21 @@ const CourseList = () => {
   return (
     <div className='admin--dashboard__list'>
       <h3 className='admin--list__header'>Papers</h3>
-      <ul></ul>
+      <ul>
+        {courses.map((course) => (
+          <li key={course._id}>
+            <div className='admin--list'>
+              <div className='list--header'>{course.name}</div>
+              <div className='list--details'>{course.courseCode}</div>
+            </div>
+            <IconContext.Provider value={{ color: 'red', size: '1.2rem', title: "Delete "}}>
+              <div className='list--delete'>
+                <AiOutlineDelete />
+              </div>
+            </IconContext.Provider>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
